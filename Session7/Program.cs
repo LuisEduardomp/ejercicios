1b4e4ad2eadc06d366c3b4a917499a76a2276a3f
LuisEduardomp <luismp100901@gmail.com>
2023-03-21 19:45:20 -0600

    carpeta practicas

---
+++ Session7/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Segunda linea sin salto");
+
+for(int i = 0; i<=10; i++)
+{
+    Console.WriteLine(i);
+}
+
+int  numeroPersonas  =  11;
+
+string  NombrePersona  =  "Luis Eduardo";
+
+Console.WriteLine ( " En este room hay {0} personas y una se llama {1} " , numeroPersonas , NombrePersona );
